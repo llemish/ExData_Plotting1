816ce22a9af7b318467aa8cdc39bd1d889bb626f
lemish <llemish@gmail.com>
2021-01-08 10:42:28 +0300

    add "plot2.R" and "plot2.png" (mark that this part was done on linux PC, so file adresses has another format!)

--- plot2.R
+++ plot2.R
@@ -1,20 +1,16 @@
 plot2 <- function() {
-        file_name <- ".//exdata_data_household_power_consumption//household_power_consumption.txt"
+        file_name <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
         full_data <- read.table(file_name, header = TRUE, sep = ";", na.strings = "?")
         
         two_day_data <- subset(full_data, (full_data$Date == "1/2/2007" | full_data$Date == "2/2/2007"))
         
-        two_day_data$Date <- as.Date(two_day_data$Date, "%d/%m/%y")
+        two_day_data$DateTime <- as.POSIXlt(paste(two_day_data$Date, two_day_data$Time), tz="",
+                                            "%d/%m/%Y %H:%M:%S")
         
-        two_day_data$Time <- strptime(two_day_data$Time, format = "%H:%M:%S")
+        png(filename = "plot2.png")
         
-        print(head(two_day_data$Date))
-        print(tail(two_day_data$Time))
-        
-        # png(filename = "plot2.png")
-        
-        # hist(two_day_data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)",
-        #      main = "Global Active Power")
-        
-        # dev.off()
+        plot(two_day_data$DateTime, two_day_data$Global_active_power, type = 'l',
+             xlab = "", ylab = "Global Active Power (kilowatts)")
+
+        dev.off()
 }
