601be54cec0cc5964c2c1451f0afdaa76eb31633
lemish <llemish@gmail.com>
2021-01-08 11:22:02 +0300

    add "plot3.R" and "plot3.png" mark that these files were created on linux PC, so file pathes are different

---
+++ plot3.R
@@ -0,0 +1,22 @@
+plot3 <- function() {
+    file_name <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
+    full_data <- read.table(file_name, header = TRUE, sep = ";", na.strings = "?")
+    
+    two_day_data <- subset(full_data, (full_data$Date == "1/2/2007" | full_data$Date == "2/2/2007"))
+    
+    two_day_data$DateTime <- as.POSIXlt(paste(two_day_data$Date, two_day_data$Time), tz="",
+                                        "%d/%m/%Y %H:%M:%S")
+    
+    png(filename = "plot3.png")
+    
+    plot(two_day_data$DateTime, two_day_data$Sub_metering_1, type = 'l',
+         xlab = "", ylab = "Energy sub metering")
+    lines(two_day_data$DateTime, two_day_data$Sub_metering_2, type = 'l',
+         xlab = "", col = 'red')
+    lines(two_day_data$DateTime, two_day_data$Sub_metering_3, type = 'l',
+          xlab = "", col = 'blue')
+    legend('topright', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
+           col = c('black', 'red', 'blue'), lty = 1)
+    
+    dev.off()
+}
